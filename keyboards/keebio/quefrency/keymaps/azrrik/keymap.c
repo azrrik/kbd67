8d0aa3183e049a5bb4e9695853b47d13defacfe5
azrrik <azrrik@gmail.com>
2021-04-13 12:28:44 -0400

    updates

--- keyboards/keebio/quefrency/keymaps/azrrik/keymap.c
+++ keyboards/keebio/quefrency/keymaps/azrrik/keymap.c
@@ -25,17 +25,30 @@ extern keymap_config_t keymap_config;
 // };
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-  [_BASE] = LAYOUT_65_with_macro(
-    TG_L3,   KC_ESC,  KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_INS,  KC_DEL, KC_HOME, \
-    KC_F1,   KC_F2,   KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_BSLS, KC_END, \
-    KC_F3,   KC_F4,   KC_CAPS, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, KC_ENT,  KC_PGUP, \
-    KC_F5,   KC_F6,   KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT, KC_UP,   KC_PGDN, \
-    KC_F7,   KC_F8,   KC_LCTL, KC_LGUI, KC_LALT, BS_L1,   KC_LCTL,          _______, SP_L2,   KC_RALT, KC_RGUI, KC_RCTL, KC_LEFT, KC_DOWN, KC_RGHT
-  ),
+    /*
+    * ┌───┬───┐  ┌───┬───┬───┬───┬───┬───┬───┐   ┌───┬───┬───┬───┬───┬───┬───┬───┬───┐
+    * | ◯ |esc|  │ ` │ 1 │ 2 │ 3 │ 4 │ 5 │ 6 │   │ 7 │ 8 │ 9 │ 0 │ - │ = │ins│del│ ◯ |
+    * ├───┼───┤  ├───┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┘ ┌─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴───┼───┤
+    * | F1|F2 |  │ tab │ q │ w │ e │ r │ t │   │ y │ u │ i │ o │ p │ [ │ ] │  \  │pUp|
+    * ├───┼───┤  ├─────┴┬──┴┬──┴┬──┴┬──┴┬──┴┐  └┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴─────┼───┤
+    * | F3|F4 |  │ cap  │ a │ s │ d │ f │ g │   │ h | j │ k │ l │ ; │ ' │  enter │pUp|
+    * ├───┼───┤  ├──────┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┐ └─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴────┬───┼───┤
+    * | F5|F6 |  │  shift │ z │ x │ c │ v │ b │   │ n │ m │ < │ > │ / │shift │ ^ │pDn|
+    * ├───┼───┤  ├────┬───┴┬──┴─┬─┴───┴──┬┴───┤   ├───┴───┴──┬┴──┬┴──┬┴──┬───┼───┼───┤
+    * | F7|F8 |  │esc |gui │alt │bs / L1 │ctl │   │ sp / L2  │alt│ctl│gui│ < │ v │ > |
+    * └───┴───┘  └────┴────┴────┴────────┴────┘   └──────────┴───┴───┴───┴───┴───┴───┘
+    */
+    [_BASE] = LAYOUT_65_with_macro(
+        TG_L3,   KC_ESC,  KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_INS,  KC_DEL, KC_HOME, \
+        KC_F1,   KC_F2,   KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_BSLS, KC_END, \
+        KC_F3,   KC_F4,   KC_CAPS, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, KC_ENT,  KC_PGUP, \
+        KC_F5,   KC_F6,   KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT, KC_UP,   KC_PGDN, \
+        KC_F7,   KC_F8,   KC_ESC,  KC_LGUI, KC_LALT, BS_L1,   KC_LCTL,          _______, SP_L2,   KC_RALT, KC_RCTL, KC_RGUI, KC_LEFT, KC_DOWN, KC_RGHT
+    ),
 
   [_L1] = LAYOUT_65_with_macro(
     RESET,   _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, RESET, \
-    _______, _______, _______, _______, _______, _______, _______, _______, _______, KC_HOME, KC_UP,   KC_END,  KC_PGUP, _______, _______, _______, _______, \
+    _______, _______, _______, _______, _______, _______, _______, _______, _______, KC_HOME, KC_UP,   KC_END,  KC_PGUP, _______, _______, _______, _______ , \
     _______, _______, _______, KC_LSFT, KC_LGUI, KC_LALT, KC_LCTL, _______, _______, KC_LEFT, KC_DOWN, KC_RGHT, KC_PGDN, _______, _______, _______, \
     KC_F9,   KC_F10,  UNDO,    CUT,     COPY,    PASTE,   _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, \
     KC_F11,  KC_F12,  _______, _______, _______, _______, _______,          KC_DEL,  _______, _______, _______, _______, _______, _______, _______
